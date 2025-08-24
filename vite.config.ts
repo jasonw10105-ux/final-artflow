bc485073189c2688a8980e973807cbab7db3f94f
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-24 14:49:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // <-- Import the 'path' module
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  /* --- ADD THIS SECTION --- */
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  /* ------------------------ */
 })
