582424a1064bf6f9ff138436d2b51aec4a5be8c1
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-24 20:25:59 +0200

    Update MarketingLayout.tsx

--- src/components/layout/MarketingLayout.tsx
+++ src/components/layout/MarketingLayout.tsx
@@ -2,13 +2,13 @@
 
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './Header'; // Import the new intelligent header
+import Header from './Header'; // This import will now succeed
 
 const MarketingLayout = () => {
     return (
         <div>
             <Header />
-            <main>
+            <main style={{ padding: '2rem' }}>
                 <Outlet />
             </main>
             {/* You can add a consistent footer here if you wish */}
